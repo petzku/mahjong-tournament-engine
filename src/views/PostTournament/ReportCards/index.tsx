deba3a7f228f3947889f0726aff2ab926ff8df2b
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2022-08-27 11:06:10 +0300

    add cumulative points chart

--- src/views/PostTournament/ReportCards/index.tsx
+++ src/views/PostTournament/ReportCards/index.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Game, Seat, Standing } from "../../../data-types/tournament-data-types";
+import { Game, Score, Seat, Standing } from "../../../data-types/tournament-data-types";
 import { State } from "../../../state";
 import { generateArray } from "../../../utils/generateArray";
 import { getStandings } from "../../../utils/getStandings";
@@ -12,7 +12,8 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
-  ReferenceLine
+  ReferenceLine,
+  Legend
 } from "recharts";
 
 import PostTabs from "../PostTabs";
@@ -31,14 +32,14 @@ type PositionData = {
 const ReportCards = () => {
   const tournamentState = useSelector((state: State) => state.tournament);
   
-  const selectedPlayerId = 5;
+  const selectedPlayerId = 15;
   const playerName = tournamentState.playerNames[selectedPlayerId];
 
   // Data format for recharts: Array of objects for each round. Object contains round label ("name") and 
   //  a property for each player's score on that round.
  
-  //For rankin evolution, generate array of all standings from all rounds. 1) Loop through all rounds.
-  const rankingEvolution = useMemo(() => generateArray(tournamentState.info.rounds).map((round: number) => (
+  //For ranking evolution, generate array of all standings from all rounds. 1) Loop through all rounds.
+  const ranking = useMemo(() => generateArray(tournamentState.info.rounds).map((round: number) => (
     // 2) Get standings for current round.
     // 3) Reformat standings into recharts data format.
     getStandings({tournament: tournamentState, atRound: round}).reduce((carry: any, current: Standing, _: number, array: Standing[]) => ({
@@ -61,13 +62,42 @@ const ReportCards = () => {
         .findIndex((seat: Seat) => seat.playerId === selectedPlayerId)
     }));
 
-    const mean = evolution.reduce((carry: number, point: PositionDataPoint) => carry+point.position, 0) / tournamentState.info.rounds;
+    const mean = evolution.reduce((carry: number, point: PositionDataPoint): number => carry+point.position, 0) / tournamentState.info.rounds;
 
     return {
       evolution,
       mean
     };
-  }, [])
+  }, []);
+
+  //For cumulative points chart, reduce game of that round and earlier rounds into a score sum.
+  const cumulativePoints = useMemo(() => generateArray(tournamentState.info.rounds).map((round: number): Score => (
+    tournamentState.games.reduce((carry: Score, game: Game): Score => {
+      const seat = game.participants.findIndex((seat: Seat): boolean => seat.playerId === selectedPlayerId);
+
+      //Skip the games that the player didn't play in, and games that aren't in the range of round
+      if (game.round > round || seat === -1)
+      {
+        return carry;
+      }
+
+      //Otherwise the player did play in this game, so we sum their score from this game to the carried sum.
+      return {
+        raw: carry.raw + game.participants[seat].score.raw,
+        uma: carry.uma + game.participants[seat].score.uma,
+        penalty: carry.penalty + game.participants[seat].score.penalty,
+      }
+    }, {raw: 0, uma: 0, penalty: 0})
+  )).map((cumulatedPoints: Score, round: number) => ({
+    name: `${round + 1}`,
+    total: cumulatedPoints.raw + cumulatedPoints.uma + cumulatedPoints.penalty,
+    ...cumulatedPoints
+  })), []);
+
+  //TODO:
+  //- highest and lowest single
+  //- mean and distribution
+  //- list of all the player's hanchans and points
 
   return (
     <div>
@@ -79,7 +109,7 @@ const ReportCards = () => {
         <LineChart
           width={700}
           height={300}
-          data={rankingEvolution}>
+          data={ranking}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis reversed={true}/>
@@ -107,6 +137,38 @@ const ReportCards = () => {
           <ReferenceLine y={positions.mean} stroke="#7777ff" />
           <Line
             dataKey={"position"}
+            stroke={"#ff0000"}
+            dot={true}
+          />
+        </LineChart>
+      </div>
+      <div>
+        <h3>Cumulative points</h3>
+        <LineChart
+          width={700}
+          height={350}
+          data={cumulativePoints}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis/>
+          <Legend/>
+          <Line
+            dataKey={"raw"}
+            stroke={"#00aa00"}
+            dot={true}
+          />
+          <Line
+            dataKey={"uma"}
+            stroke={"#0000ff"}
+            dot={true}
+          />
+          <Line
+            dataKey={"penalty"}
+            stroke={"#000000"}
+            dot={true}
+          />
+          <Line
+            dataKey={"total"}
             stroke={"#ff0000"}
             dot={true}
           />
