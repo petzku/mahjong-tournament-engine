a3d26fc4d8af0dd7d1341a5a3cfc39ef9ace1568
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2022-08-28 11:13:12 +0300

    add duplicate names guard

--- src/views/NewTournament/PlayerEntry/index.tsx
+++ src/views/NewTournament/PlayerEntry/index.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { bindActionCreators } from "redux";
+import Popup from "../../../components/Popup";
+import { PlayerName } from "../../../data-types/tournament-data-types";
 import { tournamentActionCreators } from "../../../state";
 import { Routes } from "../../../utils/findRoute";
 
@@ -10,22 +12,48 @@ import styles from "./PlayerEntryView.module.css";
 
 const PlayerEntryView = () => {
   const [playersInput, setPlayersInput] = useState<string>("");
+  const [duplicates, setDuplicates] = useState<PlayerName[]>([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   
   const {addPlayers} = bindActionCreators(tournamentActionCreators, dispatch)
   
-  const players = playersInput.split("\n").filter(name => name !== "");
+  const players: PlayerName[] = playersInput.split("\n").filter(name => name !== "");
 
   const rightAmount = players.length > 0 && players.length % 4 === 0;
 
   const saveAndContinue = (): void => {
+    const duplicatesFromInput = players.filter((name: PlayerName, index: number) => players.indexOf(name) !== index);
+
+    if (duplicatesFromInput.length > 0)
+    {
+      setDuplicates(duplicatesFromInput);
+      return;
+    }
+
     addPlayers(players);
     navigate(Routes.TableEntry);
   };
 
   return (
     <div>
+      {
+        duplicates.length > 0 &&
+        <Popup
+          title={"Duplicate players"}
+          cancelHidden={true}
+          cancelText={""}
+          onCancel={() => {}}
+          confirmText={"Ok"}
+          onConfirm={(): void => setDuplicates([])}>
+          <p>Please add some uniqueness (e.g. middle initial, nickname or city) to the names of these players:</p>
+          <ul>
+            {
+              duplicates.map((name: PlayerName) => <li key={`duplicate-${name}`}>{name}</li>)
+            }
+          </ul>
+        </Popup>
+      }
       <p>Enter players, one per line. Currently {players.length} players.</p>
       <textarea
         className={styles.playerEntry}
