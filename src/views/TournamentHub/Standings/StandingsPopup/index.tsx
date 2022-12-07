2b336df889808831f7900ae7e2c85ba927527cd2
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2022-12-07 09:47:10 +0200

    Show ranking changes between rounds

--- src/views/TournamentHub/Standings/StandingsPopup/index.tsx
+++ src/views/TournamentHub/Standings/StandingsPopup/index.tsx
@@ -1,10 +1,30 @@
+import { useSearchParams } from "react-router-dom";
+import Comparison from "../../../../components/Comparison";
 import { Standing, Tournament } from "../../../../data-types/tournament-data-types";
 import { formatPoints } from "../../../../utils/formatPoints";
+import { generateArray } from "../../../../utils/generateArray";
 import { getStandings } from "../../../../utils/getStandings";
 import styles from "./StandingsPopup.module.css";
 
 const StandingsPopup = () => {
+  const [searchParams] = useSearchParams();
+  const afterRound = parseInt(searchParams.get("afterRound") as string);
   const tournament: Tournament = JSON.parse(localStorage.getItem("mahjong-tournament") as string);
+
+  const everyRound = generateArray(tournament.info.rounds).map((round: number) => (
+    getStandings({tournament: tournament, atRound: round})
+  ));
+
+  const comparisons = generateArray(tournament.playerNames.length).map((playerId: number) => {
+    if (afterRound === 0)
+    {
+      return 0;
+    }
+
+    const previousPlacement = everyRound[afterRound - 1].findIndex((standing: Standing) => standing.playerId === playerId);
+    const currentPlacement = everyRound[afterRound].findIndex((standing: Standing) => standing.playerId === playerId);
+    return previousPlacement - currentPlacement;
+  });
 
   return (
     <div>
@@ -15,6 +35,7 @@ const StandingsPopup = () => {
             <th>Pos.</th>
             <th>Player</th>
             <th>Total</th>
+            <th>{null}</th>
           </tr>
         </thead>
         <tbody>
@@ -24,6 +45,13 @@ const StandingsPopup = () => {
                 <td className={styles.cell}>{index + 1}.</td>
                 <td className={styles.cell}>{tournament.playerNames[standing.playerId]}</td>
                 <td className={styles.cell}>{formatPoints({points: standing.points, sign: true})}</td>
+                {
+                  comparisons[standing.playerId] !== 0
+                  ?
+                  <Comparison change={comparisons[standing.playerId]}/>
+                  :
+                  <td>{null}</td>
+                }
               </tr>
             ))
           }
--- src/views/TournamentHub/Standings/index.tsx
+++ src/views/TournamentHub/Standings/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { State } from "../../../state";
 import { Standing } from "../../../data-types/tournament-data-types";
@@ -6,37 +6,77 @@ import { Standing } from "../../../data-types/tournament-data-types";
 import { formatPoints } from "../../../utils/formatPoints";
 import { getStandings } from "../../../utils/getStandings";
 import HubTabs from "../HubTabs";
+import Dropdown, {DropdownItem} from "../../../components/Dropdown";
+import Comparison from "../../../components/Comparison";
+import { generateArray } from "../../../utils/generateArray";
 
 const Standings = () => {
   const tournamentState = useSelector((state: State) => state.tournament);
+  const [afterRound, setAfterRound] = useState<number>(tournamentState.info.rounds - 1);
   const [standingsWindow, setStandingsWindow] = useState<WindowProxy | null>(null);
 
   const openWindow = () => {
     setStandingsWindow(window.open(
-      "/hub/standings/popup",
+      `/hub/standings/popup?afterRound=${afterRound}`,
       "standingsWindow",
       "width=500,height=500"
     ));
   };
 
+  const roundOptions = generateArray(tournamentState.info.rounds).map((roundId: number): DropdownItem => ({
+    value: roundId,
+    text: `Round ${roundId + 1}`
+  }));
+
+  const everyRound = useMemo(() => generateArray(tournamentState.info.rounds).map((round: number) => (
+    getStandings({tournament: tournamentState, atRound: round})
+  )), []);
+
+  const comparisons = generateArray(tournamentState.playerNames.length).map((playerId: number) => {
+    if (afterRound === 0)
+    {
+      return 0;
+    }
+
+    const previousPlacement = everyRound[afterRound - 1].findIndex((standing: Standing) => standing.playerId === playerId);
+    const currentPlacement = everyRound[afterRound].findIndex((standing: Standing) => standing.playerId === playerId);
+    return previousPlacement - currentPlacement;
+  });
+
   return (
     <div>
       <HubTabs/>
+      <Dropdown
+        label={"Show standings after round"}
+        items={roundOptions}
+        value={afterRound}
+        onChange={(newValue) => setAfterRound(newValue)}
+      />
       <button onClick={() => openWindow()}>Open in popup.</button>
       <table
         className={"standings"}>
         <thead>
           <tr>
+            <th>Pos.</th>
             <th>Player</th>
             <th>Total</th>
+            <th>{null}</th>
           </tr>
         </thead>
         <tbody>
           {
-            getStandings({tournament: tournamentState, atRound: tournamentState.info.rounds}).map((standing: Standing) => (
+            everyRound[afterRound].map((standing: Standing, position: number) => (
               <tr key={`player-standing-${standing.playerId}`}>
+                <td>{position + 1}.</td>
                 <td>{tournamentState.playerNames[standing.playerId]}</td>
                 <td>{formatPoints({points: standing.points, sign: true})}</td>
+                {
+                  comparisons[standing.playerId] !== 0
+                  ?
+                  <Comparison change={comparisons[standing.playerId]}/>
+                  :
+                  <td>{null}</td>
+                }
               </tr>
             ))
           }
