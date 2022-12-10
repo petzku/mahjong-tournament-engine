793c922ca05bdfe13e289dd9515f31a2934d4196
Pauli Marttinen <48649834+PauliMarttinen@users.noreply.github.com>
2022-12-10 21:53:34 +0200

    slight tab appearance improvement

--- src/views/TournamentHub/HubTabs/index.tsx
+++ src/views/TournamentHub/HubTabs/index.tsx
@@ -1,14 +1,15 @@
 import { Routes } from "../../../utils/routeUtils";
 import { Link } from "react-router-dom";
+import styles from "./HubTabs.module.css";
 
 const HubTabs = () => {
   return (
-    <div>
-      <Link to={Routes.Schedule}>Full schedule</Link>
-      <Link to={Routes.Standings}>Standings</Link>
-      <Link to={Routes.EditPlayers}>Edit players</Link>
-      <Link to={Routes.EditTables}>Edit tables</Link>
-      <Link to={Routes.PrintOuts}>Print-outs</Link>
+    <div className={styles.hubTabs}>
+      <Link className={styles.tab} to={Routes.Schedule}>Full schedule</Link>
+      <Link className={styles.tab} to={Routes.Standings}>Standings</Link>
+      <Link className={styles.tab} to={Routes.EditPlayers}>Edit players</Link>
+      <Link className={styles.tab} to={Routes.EditTables}>Edit tables</Link>
+      <Link className={styles.tab} to={Routes.PrintOuts}>Print-outs</Link>
     </div>
   );
 };
